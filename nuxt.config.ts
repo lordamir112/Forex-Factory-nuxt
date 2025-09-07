2272c48818b3583183c5b495ef3f693b5be4ab7c
lordamir112 <78014726+lordamir112@users.noreply.github.com>
2025-09-07 13:33:38 +0330

    folders and files structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  devServer: {
+    port: 4050
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://dummyjson.com'
---
+++ pages/employees/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Nuxt 4 App ✅</h1>
+    <NuxtLink to="/employees">Go to Employees</NuxtLink>
+  </div>
+</template>
